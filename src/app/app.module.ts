153ce84f9646819425c752a25fe41838e12c0343
DanielyanAndranik <danielyandranik@gmail.com>
2018-11-13 00:40:10 +0400

    removes navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,7 +10,6 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     MainComponent,
     HeaderComponent,
     FooterComponent
